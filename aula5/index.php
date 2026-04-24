d683042ea9f1d38846c9dd68b7714d093816d997
MAX LOPES GARCIA DE OLIVEIRA <max.oliveira@cs.up.edu.br>
2026-04-24 17:56:25 -0300

    aula 5 atualizada

---
+++ aula5/index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Aula Sessões em PHP - Login</title>
+    <link rel="stylesheet" href="./assets/style.css">
+</head>
+<body>
+    <header>
+        <h1>Sessões em PHP</h1>
+    </header>
+
+    <main>
+        <form action="./admin" method="post">
+            <h2>Login</h2>
+
+            <div class="form-group">
+                <label for="login">Login:</label>
+                <input type="text" name="login" id="login" required>
+            </div>
+
+            <div class="form-group">
+                <label for="senha">Senha:</label>
+                <input type="password" name="senha" id="senha" required>
+            </div>
+
+            <button type="submit">Entrar</button>
+        </form>
+    </main>
+
+    <footer>
+        <p>Rodapé</p>
+    </footer>
+</body>
+</html>
